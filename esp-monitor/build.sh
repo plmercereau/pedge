260670902e9403aca64bb3b98b94a20499e7d550
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2024-06-12 21:15:58 +0200

    chore: right file

--- esp-monitor/build.sh
+++ esp-monitor/build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 platformio run --environment esp32dev
-# TODO only copy the right file(s)
-cp .pio/build/esp32dev/firmware.* /firmware/ 2>/dev/null
+cp .pio/build/esp32dev/firmware.bin /firmware/
