4ae4deec8a1579e630f8986652e93f7d7e245db8
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2024-06-12 21:01:22 +0200

    fix: cp

--- esp-monitor/build.sh
+++ esp-monitor/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 platformio run --environment esp32dev
 # TODO only copy the right file(s)
-cp .pio/build/esp32dev/firmware.* /build/ 2>/dev/null
+cp .pio/build/esp32dev/firmware.* /firmware/ 2>/dev/null
