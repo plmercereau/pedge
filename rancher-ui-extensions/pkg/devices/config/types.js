3a04ad4b768ff5fc9abe4604e3961c55be0771ea
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2024-07-29 21:13:47 +0200

    chore: clean misc stuff

--- rancher-ui-extensions/pkg/devices/config/types.js
+++ rancher-ui-extensions/pkg/devices/config/types.js
@@ -1,13 +1,13 @@
-export const DEFAULT_NAMESPACE = "default";
+export const DEFAULT_NAMESPACE = 'default';
 
-export const PRODUCT_NAME = "devices";
+export const PRODUCT_NAME = 'devices';
 
 export const SCHEMA_IDS = {
-  DEVICE_CLUSTERS: "devices.pedge.io.devicecluster",
-  DEVICE_CLASSES: "devices.pedge.io.deviceclass",
-  DEVICES: "devices.pedge.io.device",
+  DEVICE_CLUSTERS: 'devices.pedge.io.devicecluster',
+  DEVICE_CLASSES:  'devices.pedge.io.deviceclass',
+  DEVICES:         'devices.pedge.io.device',
 };
 
-export const DASHBOARD = "dashboard";
+export const DASHBOARD = 'dashboard';
 
-export const BLANK_CLUSTER = "_";
+export const BLANK_CLUSTER = '_';
