4d010a8db40afc9b2039555be1c8cf0d1d8a29c5
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2024-07-29 21:08:14 +0200

    feat: add columns in the ui extension

--- rancher-ui-extensions/pkg/devices/index.ts
+++ rancher-ui-extensions/pkg/devices/index.ts
@@ -1,5 +1,5 @@
 import { importTypes } from '@rancher/auto-import';
-import { IPlugin } from '@shell/core/types';
+import { IPlugin, TableColumnLocation } from '@shell/core/types';
 import extensionRouting from './routing/extension-routing';
 
 // Init the package
@@ -13,4 +13,34 @@ export default function(plugin: IPlugin) {
   plugin.addProduct(require('./product'));
   // Add Vue Routes
   plugin.addRoutes(extensionRouting);
+
+  plugin.addTableColumn(
+    TableColumnLocation.RESOURCE,
+    { resource: ['devices.pedge.io.device'] },
+    {
+      name:     'device-class-col',
+      labelKey: 'devices.deviceClass.label',
+      getValue: (row: any) => {
+        return row.spec?.deviceClassReference?.name;
+      },
+      width:  150,
+      sort:   ['stateSort', 'nameSort'],
+      search: ['stateSort', 'nameSort'],
+    }
+  );
+
+  plugin.addTableColumn(
+    TableColumnLocation.RESOURCE,
+    { resource: ['devices.pedge.io.deviceclass'] },
+    {
+      name:     'device-cluster-col',
+      labelKey: 'devices.deviceCluster.label',
+      getValue: (row: any) => {
+        return row.spec?.deviceClusterReference?.name;
+      },
+      width:  150,
+      sort:   ['stateSort', 'nameSort'],
+      search: ['stateSort', 'nameSort'],
+    }
+  );
 }
